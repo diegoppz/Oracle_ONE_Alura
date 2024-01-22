1e134b92b13a653e89f88ac0ad4609a20468f604
Diego Umbelino <diego9us@gmail.com>
2024-01-22 17:27:45 -0300

    teste correção

--- initLogicaProgramacaoJavascript/NumberSecretGame/logica-js-projeto_inicial/app.js
+++ initLogicaProgramacaoJavascript/NumberSecretGame/logica-js-projeto_inicial/app.js
@@ -1,4 +1,10 @@
 
 alert('Bem vind@ ao jogo do núero secreto')
 let numeroSecreto = 30;
+let chute = prompt('Escolha um número entre 0 e 30');
+
+if (numeroSecreto == chute) {
+console.log('É isso ai! você descobriu o número secreto (30)
+}
+
 
